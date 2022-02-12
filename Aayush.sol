1b7166410c1c8e68763e9a8fef178e75ee8808ce
AayushGrover101 <78941537+AayushGrover101@users.noreply.github.com>
2022-02-12 09:44:54 -0500

    Update Aayush.sol

--- Aayush.sol
+++ Aayush.sol
@@ -8,6 +8,7 @@ contract Aayush is ERC20 {
 
     constructor() ERC20('Aayush Token', 'AAY') {
         _mint(msg.sender, 10000 * 10 ** 18);
+        admin = msg.sender;
     }
 
     function mint(address to, uint amount) external {
